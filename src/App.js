9233badb5fce4cb5cc6b12c60a40a6216baed5e6
jw <jiewenw96@gmail.com>
2021-02-26 22:46:47 -0500

    1

--- src/App.js
+++ src/App.js
@@ -2,57 +2,16 @@ import { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const TOKEN = "5bc72686da814f75abbae295097e3047"
+const API_URL = "http://lightsail.jiewen.wang:3001/quickswap"
 
-const UNISWAP_API = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2"
+const INTERVAL = 20000
 
-const QUICKSWAP_API = "https://api.thegraph.com/subgraphs/name/sameepsi/quickswap"
-
-const INTERVAL = 8000
-
-const watchlist = [
-  {
-    name: 'SWAP-ETH',
-    quickswap: '0xba8a6d86cd5577426ffbea6c40b7334650ff3900',
-    uniswap: '0xd90a1ba0cbaaaabfdc6c814cdf1611306a26e1f8',
-    inverse: true
-  },
-  {
-    name: 'USDC-ETH',
-    quickswap: '0x853ee4b2a13f8a742d64c8f088be7ba2131f670d',
-    uniswap: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc'
-  },
-  {
-    name: 'MATIC-ETH',
-    quickswap: '0xadbf1854e5883eb8aa7baf50705338739e558e5b',
-    uniswap: '0x819f3450da6f110ba6ea52195b3beafa246062de'
-  }
-]
-
-async function fetch_subgraph(api, pair_address) {
-  const query = `
-    query {
-      pair_price: pair(id: "${pair_address}"){
-        token0Price
-        token1Price
-      }
-    }`;
-  var res = await fetch(api, {
-      method: 'POST',
-      body: JSON.stringify({query}),
-      headers: {
-          'Authorization': `Bearer ${TOKEN}`,
-      },
-  })
+async function fetch_prices() {
+  var res = await fetch(API_URL)
   
   try {
       var body = await res.json()
-      var token0Price = parseFloat(body.data.pair_price.token0Price)
-      var token1Price = parseFloat(body.data.pair_price.token1Price)
-      return { 
-        token0Price: token0Price, 
-        token1Price: token1Price
-      }
+      return body
   }
   catch (err) {
       console.error(err)
@@ -85,42 +44,45 @@ function App() {
   const [priceData, setPriceData] = useState({uniswap: {}, quickswap: {}})
 
   const refreshData = () => {
-    watchlist.forEach(item => {
-      fetch_subgraph(UNISWAP_API).then(result => {
-        if (result) {
-          var newPriceData = priceData
-          newPriceData.uniswap[item.name] = item.inverse? [result.token1Price, result.token0Price] : [result.token0Price, result.token1Price]
-          setPriceData(newPriceData)
-        }
-      })
-
-      fetch_subgraph(QUICKSWAP_API).then(result => {
-        if (result) {
-          var newPriceData = priceData
-          newPriceData.quickswap[item.name] = [result.token0Price, result.token1Price]
-          setPriceData(newPriceData)
-        }
-      })
+    fetch_prices().then(result => {
+      setPriceData(result)
+      console.log(result)
     })
   }
+
+  useEffect(refreshData, [])
 
   useInterval(refreshData, INTERVAL)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {JSON.stringify(priceData)}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Pair</th>
+                <th>Quickswap 0</th>
+                <th>Uniswap 0</th> 
+                <th>Quickswap 1</th>
+                <th>Uniswap 1</th>
+                <th>Diff</th>
+              </tr>
+              {
+                Object.keys(priceData).map(key => (
+                    <tr>
+                      <th>{key}</th>
+                      <th>{priceData[key].quickswap ? priceData[key].quickswap.token0Price : "None"}</th>
+                      <th>{priceData[key].uniswap ? priceData[key].uniswap.token0Price : "None"}</th>
+                      <th>{priceData[key].quickswap ? priceData[key].quickswap.token1Price : "None"}</th>
+                      <th>{priceData[key].uniswap ? priceData[key].uniswap.token1Price : "None"}</th>
+                      <th>{priceData[key].diff}</th>
+                    </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
